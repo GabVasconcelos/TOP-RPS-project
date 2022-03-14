accb0ac59fcb4a142f159667f0b8c35a50ccdb42
GabVasconcelos <gabrielvasconcelos2709@gmail.com>
2022-03-14 20:11:38 -0300

    Add game function

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ const options = {
 
 function playRound(selection) {
     let results = computeRound(selection.toLowerCase())
-    console.log(`You played ${results[0]} while the opponent played ${results[1]}, you ${results[2] && "won!" || "lost."}`)
+    return `You played ${results[0]} while the opponent played ${results[1]}, you ${results[2] && "won!" || "lost."}`
 }
 
 function computeRound(selection) {
-    let counter = options[selection]
+    let counter = options[selection.toLowerCase()]
     let computerSelection = computerPlay()
     let result = computerSelection != counter && computerSelection != selection
 
@@ -20,3 +20,11 @@ function computeRound(selection) {
 function computerPlay() {
     return Object.keys(options)[Math.floor(Math.random() * Object.keys(options).length)]
 }
+
+function game() {
+    for (let i=0; i<5; i++) {
+        alert(playRound(prompt()))
+    }
+}
+
+game()
