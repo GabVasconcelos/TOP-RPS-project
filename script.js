4e441b2b984ff6271872ff72c66ce46b815c5fc5
GabVasconcelos <gabrielvasconcelos2709@gmail.com>
2022-03-14 20:05:16 -0300

    Change order of return values from computeRound and change function order

--- script.js
+++ script.js
@@ -4,20 +4,19 @@ const options = {
     "scissors": "rock"
 }
 
+function playRound(selection) {
+    let results = computeRound(selection.toLowerCase())
+    console.log(`You played ${results[0]} while the opponent played ${results[1]}, you ${results[2] && "won!" || "lost."}`)
+}
+
 function computeRound(selection) {
     let counter = options[selection]
     let computerSelection = computerPlay()
     let result = computerSelection != counter && computerSelection != selection
 
     return [selection, computerSelection, result]
-
 }
 
 function computerPlay() {
     return Object.keys(options)[Math.floor(Math.random() * Object.keys(options).length)]
-}
-
-function playRound(selection) {
-    let results = computeRound(selection.toLowerCase())
-    console.log(`You played ${results[0]} while the opponent played ${results[1]}, you ${results[2] && "won!" || "lost."}`)
 }
