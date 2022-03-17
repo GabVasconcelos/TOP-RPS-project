37d48cc59747c08a42889b044ce21710701122cc
GabVasconcelos <gabrielvasconcelos2709@gmail.com>
2022-03-17 20:44:52 -0300

    Integrate script with new buttons and text boxes

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
+const scoretext = document.querySelector(".score")
+const lastplay = document.querySelector(".lastplay")
 const options = {
     "rock": "paper",
     "paper": "scissors",
     "scissors": "rock"
 }
 
+const score = {
+    player: 0,
+    bot: 0,
+}
+
 function playRound(selection) {
     let results = computeRound(selection.toLowerCase())
-    return `You played ${results[0]} while the opponent played ${results[1]}, you ${results[2] && "won!" || "lost."}`
+    if (!results[3]) score[results[2] && "player" || "bot"]++
+    scoretext.innerHTML = `Score: <b> You: ${score.player} - Bot: ${score.bot} </b>`
+    lastplay.innerHTML = `You played ${results[0]} while the opponent played ${results[1]}, you ${results[3] && "drew." || results[2] && "won!" || "lost."}`
+    console.table(score)
+
+    return results
+    // return `You played ${results[0]} while the opponent played ${results[1]}, you ${results[2] && "won!" || "lost."}`
 }
 
 function computeRound(selection) {
     let counter = options[selection.toLowerCase()]
     let computerSelection = computerPlay()
     let result = computerSelection != counter && computerSelection != selection
-
-    return [selection, computerSelection, result]
+    let draw = computerSelection == selection
+    return [selection, computerSelection, result, draw]
 }
 
 function computerPlay() {
@@ -27,4 +40,8 @@ function game() {
     }
 }
 
-game()
+function clicked(arg) {
+    playRound(arg)
+}
+
+// game()
